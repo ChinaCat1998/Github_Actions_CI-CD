d641d800fad982d0a4c0d83bdd98f96e81c69d5e
Catrina_Evilsizer <tranqcalypso@gmail.com>
2024-11-14 20:19:25 -0500

    initial commit

---
+++ server/src/seeds/cleanDb.ts
@@ -0,0 +1,19 @@
+import models from '../models/index.js';
+import db from '../config/connection.js';
+
+export default async (modelName: "Question", collectionName: string) => {
+  try {
+    const model = models[modelName];
+    if (model && model.db && model.db.db) {
+      let modelExists = await model.db.db.listCollections({
+        name: collectionName
+      }).toArray()
+
+      if (modelExists.length) {
+        await db.dropCollection(collectionName);
+      }
+    }
+  } catch (err) {
+    throw err;
+  }
+}
